347f20417e5250f8fbdd353f85584ed1fc1a1c94
Makarov Anton <ieee@mail.ru>
2017-09-08 15:31:43 +0300

    Delete prefs.js

--- prefs.js
+++
@@ -1,2 +0,0 @@
-user_pref("browser.startup.homepage", "https://ru.search.yahoo.com/?fr=vmn&type=vmn__webcompa__1_0__ya__hp_WCYID10041_doc2pdf_151114__yaff");
-user_pref("browser.newtab.url", "https://ru.search.yahoo.com/?fr=vmn&type=vmn__webcompa__1_0__ya__hp_WCYID10041_doc2pdf_151114__yaff");
